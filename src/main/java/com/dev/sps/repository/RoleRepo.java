4633b49ebb0929899e3c909d97fefb0378ef8b2a
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-07-08 17:42:45 +0300

    add initialization data

--- src/main/java/com/dev/sps/repository/RoleRepo.java
+++ src/main/java/com/dev/sps/repository/RoleRepo.java
@@ -3,12 +3,11 @@ package com.dev.sps.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.dev.sps.models.Feedback;
 import com.dev.sps.models.Role;
 
 
 public interface RoleRepo extends 
-PagingAndSortingRepository<Feedback, Integer>,
-CrudRepository<Feedback, Integer>{
+PagingAndSortingRepository<Role, Integer>,
+CrudRepository<Role, Integer>{
     
 }
