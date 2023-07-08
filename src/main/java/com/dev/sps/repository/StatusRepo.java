b7e82d9526666867b01d3def8dd9641cf8eb6541
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-07-08 16:25:25 +0300

    add repos

--- src/main/java/com/dev/sps/repository/StatusRepo.java
+++ src/main/java/com/dev/sps/repository/StatusRepo.java
@@ -1,5 +1,14 @@
 package com.dev.sps.repository;
 
-public class StatusRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+import com.dev.sps.models.Status;
+
+
+public interface StatusRepo extends 
+PagingAndSortingRepository<Status, Integer>,
+CrudRepository<Status, Integer> {
     
 }
--- src/main/java/com/dev/sps/repository/UserRepo.java
+++ src/main/java/com/dev/sps/repository/UserRepo.java
@@ -1,5 +1,11 @@
 package com.dev.sps.repository;
 
-public class UserRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.dev.sps.models.User;
+public interface UserRepo extends 
+PagingAndSortingRepository<User, Integer>,
+CrudRepository<User, Integer>{
     
 }
